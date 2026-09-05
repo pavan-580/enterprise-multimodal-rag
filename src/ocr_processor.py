f1240ba51db4d2f4c23a7f8701d11984f1768698
Pavan Sai <pavansai580@gmail.com>
2026-09-05 14:24:08 +0530

    Add cloud deployment support

--- src/ocr_processor.py
+++ src/ocr_processor.py
@@ -1,14 +1,18 @@
+import os
+
 import pytesseract
 from PIL import Image
 from pathlib import Path
+
 from src.error_handler import setup_logging, log_error
 
-# Tell pytesseract exactly where Tesseract is installed
-pytesseract.pytesseract.tesseract_cmd = (
-    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-)
 
+setup_logging()
 
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    )
 def extract_text_from_image(image_path):
     """
     Extract text from an image using Tesseract OCR.
